2c61f43b30f9a21e654d17ebd4b32ccae0057c3c
eabenzac <166520846+eabenzac@users.noreply.github.com>
2025-02-14 17:09:22 +0100

    Add files via upload

---
+++ ExerciciResoltJava/CodiResolt.java
@@ -0,0 +1,35 @@
+package junit;
+
+import java.util.Scanner;
+
+public class Codi {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//int casos = sc.nextInt();
+		//for (int i =0;i<casos;i++) {
+			Double num1 = sc.nextDouble();
+			Double num2 = sc.nextDouble();
+			System.out.println(filipinos(num1,num2));
+		//}
+		
+
+	}
+	public static String filipinos(double num1,double num2) {
+		if (num1 <0) {
+			if (num2 <= (num1*-0.5) || num2 <= num1) {
+				return "SI";
+			}else {
+				return "NO";
+			}
+		}else {
+			if (num2 < (num1*1.5) || num2 <= num1 ) {
+				return "SI";
+			}else {
+				return "NO";
+			}
+		}
+		
+	}
+
+}
