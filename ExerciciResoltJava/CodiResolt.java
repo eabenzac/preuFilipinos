f58edfdbddf9a5b08bc39230629d229a9831b1f8
eabenzac <166520846+eabenzac@users.noreply.github.com>
2025-02-14 17:11:20 +0100

    Add files via upload

--- ExerciciResoltJava/CodiResolt.java
+++ ExerciciResoltJava/CodiResolt.java
@@ -6,12 +6,12 @@ public class Codi {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//int casos = sc.nextInt();
-		//for (int i =0;i<casos;i++) {
+		int casos = sc.nextInt();
+		for (int i =0;i<casos;i++) {
 			Double num1 = sc.nextDouble();
 			Double num2 = sc.nextDouble();
 			System.out.println(filipinos(num1,num2));
-		//}
+		}
 		
 
 	}
